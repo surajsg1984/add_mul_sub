726978f711c0a0f51437a93aaacd7ef072250673
surajsg1984 <surajsuryavanshi2003@gmail.com>
2025-04-03 15:31:10 +0530

    Update add.java
    correction of print

--- add.java
+++ add.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
-class calc{
-        public static void main(String[] suraj){
-                Scanner sc = new Scanner(System.in);
-                System.out.println("enter first nummber a:");
-                int a = sc.nextInt();
-                System.out.pritln("enter seconf number b:");
-                int b = sc.nextInt();
-                int c = a+b;
-                System.out.println("addition is "+c);
 
-         }
+class Calc {  
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);  
 
+        System.out.println("Enter first number (a):");  
+        int a = sc.nextInt();  
+
+        System.out.println("Enter second number (b):");  
+        int b = sc.nextInt();  
+
+        int c = a + b;  
+        System.out.println("Addition is: " + c);  
+
+        sc.close(); // Closing Scanner to prevent memory leaks
+    }  
 }
+
 
