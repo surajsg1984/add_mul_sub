b5dde91994bc3b393f5e0028960024027cd47e8f
dev <dev@123>
2025-04-03 15:19:50 +0530

    add.java file commiting

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class calc{
+        public static void main(String[] suraj){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("enter first nummber a:");
+                int a = sc.nextInt();
+                System.out.pritln("enter seconf number b:");
+                int b = sc.nextInt();
+                int c = a+b;
+                System.out.println("addition is "+c);
+
+         }
+
+}
+
